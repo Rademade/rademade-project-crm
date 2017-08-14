58379879ff24acd976dc4789f2faa0221f7627a3
andrey-abramow <abramow.andrey@gmail.com>
2017-08-14 10:19:38 +0300

    added params to jira_hooks_url

--- backend/app/services/jira_hooks_creator.rb
+++ backend/app/services/jira_hooks_creator.rb
@@ -1,15 +1,27 @@
 class JiraHooksCreator 
 
-  include Rails.application.routes.url_helpers 
- 
+  JIRA_SPRINT_ID   = '${sprint.id}'
+  JIRA_PROJECT_KEY = '${project.key}'
   HOOKS = {
-    ['jira:sprint_closed']  => :api_jira_hooks_sprint_closed_index_url,
-    ['jira:sprint_created'] => :api_jira_hooks_sprint_created_index_url
+    ['jira:sprint_closed'] => {
+      controller: 'api/jira_hooks/sprint_closed',
+      action: :create,
+      project_key: JIRA_PROJECT_KEY,
+      sprint_id: JIRA_SPRINT_ID
+    },
+    ['jira:sprint_created'] => {
+      controller: 'api/jira_hooks/sprint_created',
+      action: :create,
+      project_key: JIRA_PROJECT_KEY,
+      sprint_id: JIRA_SPRINT_ID
+    }
   }
 
+  include Rails.application.routes.url_helpers 
+  
   def call
-    HOOKS.each do |events, url|
-      Jira::Config.client.Webhook.build.save(events: events, url: send(url))
+    HOOKS.each do |events, url_data|
+      Jira::Config.client.Webhook.build.save(events: events, url: url_for(url_data))
     end
   end
 
