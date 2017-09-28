9ccfa546191f761faac0ec681d5b7089d498ea31
andrey-abramow <abramow.andrey@gmail.com>
2017-09-28 09:22:53 +0300

    a little refactoring

--- backend/app/services/toggle/member_detail.rb
+++ backend/app/services/toggle/member_detail.rb
@@ -5,12 +5,13 @@ class Toggle::MemberDetail
   def initialize(member_detail)
     @member_detail = member_detail
   end
-  
+
   def inspect
     TimeFormatter.sec_to_h_m(toggle_time)
   end
- 
+
   def toggle_time
+    return 0 unless sprint.start_at
     @toggle_time ||= Toggle::Member.new(member).duration(sprint.start_at, (sprint.end_at || DateTime.current))
   end
 
--- frontend/src/app/components/layout/projects/list/table-item/index.jsx
+++ frontend/src/app/components/layout/projects/list/table-item/index.jsx
@@ -1,6 +1,6 @@
-import React, { Component, PropTypes } from 'react'
-import { Route } from 'react-router'
-import { Link } from 'react-router-dom'
+import React, { Component, PropTypes } from  'react'
+import { Route }                       from  'react-router'
+import { Link }                        from  'react-router-dom'
 
 const ProjectTableItem = ({ project, onDelete, onUpdate }) => (
   <tr>
