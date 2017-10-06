b096977090b4ad3dbd2d92e19572792595acc496
andrey-abramow <abramow.andrey@gmail.com>
2017-10-06 17:19:04 +0300

    add puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,15 +7,15 @@ require 'mina/npm'
 require 'mina/puma'
 set :application_name, 'rademade_crm'
 set :port, 4002
-set :forward_agent, true
 set :domain, 'vm.rademade.com'
+set :user, 'rademade-crm'
 set :deploy_to, '/home/rademade-crm/website-backend'
 set :repository, 'git@github.com:Rademade/rademade-project-crm.git'
 set :branch, 'feature/deploy'
-set :shared_files, ['.env']
-set :shared_paths, ['log']
-set :user, 'rademade-crm'
-
+set :shared_files, fetch(:shared_dirs, []).push('.env')
+set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets')
+set :forward_agent, true
+``
 ruby_version = File.read(File.join __dir__, '../.ruby-version').chomp
 ruby_gemset = File.read(File.join __dir__, '../.ruby-gemset').chomp
 
@@ -37,6 +37,7 @@ task :setup do
     fi
   )
   invoke :environment
+    # Puma needs a place to store its pid file and socket file.
   command %(gem list ^bundler$ -i || gem install bundler)
 end
 
@@ -48,7 +49,7 @@ task :deploy do
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     in_path('frontend') do
-      command 'npm install'
+      command 'npm i'
       command 'npm run deploy'
     end
     invoke :'deploy:cleanup'
